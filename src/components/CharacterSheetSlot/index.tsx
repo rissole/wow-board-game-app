a7e388fe453a6d4015eed3ebae926e5821c66497
Luke Durkan <ldurkan92@gmail.com>
2022-05-01 18:05:23 +1000

    Added in inital trail of displaying powers in character slots.

--- src/components/CharacterSheetSlot/index.tsx
+++ src/components/CharacterSheetSlot/index.tsx
@@ -1,20 +1,47 @@
 import styled from "styled-components";
+import { Power, PowerType } from "../../types";
+import activeSpellPath from "./active_spell_icon.jpg";
+import instantSpellPath from "./instant_spell_icon.jpg";
+import weaponPath from "./weapon_icon.jpg";
+import Icon from "../Icon";
 
 export interface Props {
-  equipped: boolean;
-  slotItem?: string;
-  slotAddons?: string[];
+  power?: Power;
 }
 
+const getIconPath = (powerType: PowerType) => {
+  if (powerType === "active") {
+    return activeSpellPath;
+  } else if (powerType === "instant") {
+    return instantSpellPath;
+  } else {
+    return weaponPath;
+  }
+};
+
 const CharacterSheetSlot = (props: Props) => {
-  return <Container></Container>;
+  if (props.power) {
+    return (
+      <Container>
+        <Icon height={32} width={32} path={getIconPath(props.power.type)} />
+        <div> {props.power.energyCost} </div>
+        <div style={{ width: "100px" }}> {props.power.name} </div>
+      </Container>
+    );
+  }
+  return <Container />;
 };
 
 const Container = styled.div`
   background-color: #aaa;
   width: 100vw;
-  height: 96px;
+  height: 55px;
   border: 1px solid black;
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  padding: 8px;
+  flex-grow: 0;
 `;
 
 export default CharacterSheetSlot;
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,10 @@
 export type StatType = "health" | "energy" | "gold";
 
 export type CharacterLevel = 1 | 2 | 3 | 4 | 5;
+
 export const isValidLevel = (level: number): level is CharacterLevel =>
   level === 1 || level === 2 || level === 3 || level === 4 || level === 5;
+
 export interface CharacterStats {
   health: {
     current: number;
@@ -16,3 +18,28 @@ export interface CharacterStats {
     current: number;
   };
 }
+export type HeroClass = "druid" | "hunter";
+
+export type DiceColour = "green" | "red" | "blue";
+
+export type PowerType = "active" | "instant" | "weapon";
+
+export type Phase = "End_Reroll" | "Start_Dice" | "Start_Action" | "Place_Tokens" | "Team_Health_Down" | "Global";
+
+export type Power = {
+  name: string;
+  class: HeroClass;
+  type: PowerType;
+  requiredLevel: CharacterLevel;
+  goldCost: number;
+  energyCost: number;
+  rawDescription: string;
+  iconLink: string;
+  phase: Phase;
+  dependantOn?: string[];
+  attributesImpacted?: string[];
+  effect: string;
+  spotColour?: DiceColour[];
+  spotAmount?: string;
+  maxSpotAmount?: number;
+};

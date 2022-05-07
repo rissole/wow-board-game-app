95e7a7c0cfbdc668ee2ec51fec322742e23ab2b7
Ben Wong <benwong@atlassian.com>
2022-05-07 16:53:55 +1000

    Start refactoring Footer and implement character info section

---
+++ src/components/ScreenMain/Footer/ClassText/index.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { HeroClass } from "../../../../types";
+
+interface Props {
+  heroClass: HeroClass;
+}
+
+const ClassColorText = styled.span`
+  color: ${(props) => props.color || "black"};
+`;
+
+const getClassColor = (heroClass: HeroClass) => {
+  switch (heroClass) {
+    case "paladin":
+      return "pink";
+    case "warlock":
+      return "purple";
+    case "rogue":
+      return "yellow";
+    case "mage":
+      return "cyan";
+    case "hunter":
+      return "green";
+    case "druid":
+      return "orange";
+    case "warrior":
+      return "brown";
+    case "shaman":
+      return "blue";
+    case "priest":
+      return "white";
+    default:
+      return "black";
+  }
+};
+
+const ClassText = ({ heroClass }: Props) => {
+  const text = heroClass.substring(0, 1).toUpperCase().concat(heroClass.substring(1));
+
+  return <ClassColorText color={getClassColor(heroClass)}>{text}</ClassColorText>;
+};
+
+export default ClassText;
---
+++ src/components/ScreenMain/Talents/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  text-align: center;
+  background-color: #aaa;
+  height: 96px;
+`;
+
+const Talents = () => {
+  return <Container>Talents</Container>;
+};
+
+export default Talents;

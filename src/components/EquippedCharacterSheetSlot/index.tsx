d9c56585dc0d0cfc41bd0ee6afddbd4c97df553e
Ben Wong <benwong@atlassian.com>
2022-05-22 01:46:19 +1000

    Add ability to remove an equipped card from a character sheet slot

--- src/components/EquippedCharacterSheetSlot/index.tsx
+++ src/components/EquippedCharacterSheetSlot/index.tsx
@@ -1,6 +1,10 @@
+import { useCallback, useContext, useMemo } from "react";
 import styled from "styled-components";
-import { AttributeImpact, CardSlot, Power } from "../../types";
+import useFlipFlop from "../useFlipFlop";
+import { AttributeImpact, CardSlot, Power, UniqueCardName } from "../../types";
 import Icon from "../Icon";
+import UnequipCarousel from "../CarouselUnequip";
+import { GameContext } from "../GameProvider";
 import RenderedDice from "../RenderedDice";
 import SlotTypeIcons from "../SlotTypeIcons";
 import CharacterSheetSlot from "../BaseCharacterSheetSlot";
@@ -33,31 +37,58 @@ const getBackgroundColor = (power: Power) => {
 };
 
 const EquippedCharacterSheetSlot = (props: Props) => {
-  if (props.slot.equipped[0] === undefined) {
+  const { unequipCardFromSlot } = useContext(GameContext);
+  const { value: isModalOpen, setOff: closeModal, toggle: toggleModal } = useFlipFlop(false);
+
+  /**
+   * TODO support displaying multiple cards in a slot
+   */
+  const equippedCards = useMemo(
+    () =>
+      props.slot.equipped.map((cardName) => {
+        const p = getPowerByName(cardName);
+        if (p === undefined) {
+          throw new Error(
+            `Couldn't find data for card ${props.slot.equipped[0]} in slot ${props.slot.metadata.slotNumber}`
+          );
+        }
+        return p;
+      }),
+    [props.slot.equipped, props.slot.metadata.slotNumber]
+  );
+
+  const equippedCardData = equippedCards[0];
+  if (equippedCardData === undefined) {
     throw new Error(
       `Nothing equipped in this slot you're trying to look at (slotNumber: ${props.slot.metadata.slotNumber})`
     );
   }
-  /**
-   * TODO support multiple cards in a slot
-   */
-  const equippedCardData = getPowerByName(props.slot.equipped[0]);
-  if (equippedCardData === undefined) {
-    throw new Error(`Couldn't find data for card ${props.slot.equipped[0]} in slot ${props.slot.metadata.slotNumber}`);
-  }
+
+  const handleSelectItem = useCallback(
+    (name: UniqueCardName) => {
+      unequipCardFromSlot(props.slot.metadata.slotNumber, name);
+      closeModal();
+    },
+    [closeModal, props.slot.metadata.slotNumber, unequipCardFromSlot]
+  );
 
   return (
-    <CharacterSheetSlot backgroundColor={getBackgroundColor(equippedCardData)}>
-      <Container>
-        <SlotTypeIcons slotTypes={props.slot.metadata.slotTypes} isEquippedSlot={true} />
-        <MainContent>
-          <Icon path={equippedCardData.iconLink} height={36} width={36} />
-          <CostBox>{equippedCardData.energyCost}</CostBox>
-          <NameBox>{equippedCardData.name}</NameBox>
-          <AttributesImpactedView attributesImpacted={equippedCardData.attributesImpacted} />
-        </MainContent>
-      </Container>
-    </CharacterSheetSlot>
+    <>
+      <CharacterSheetSlot backgroundColor={getBackgroundColor(equippedCardData)} onClick={toggleModal}>
+        <Container>
+          <SlotTypeIcons slotTypes={props.slot.metadata.slotTypes} isEquippedSlot={true} />
+          <MainContent>
+            <Icon path={equippedCardData.iconLink} height={36} width={36} />
+            <CostBox>{equippedCardData.energyCost}</CostBox>
+            <NameBox>{equippedCardData.name}</NameBox>
+            <AttributesImpactedView attributesImpacted={equippedCardData.attributesImpacted} />
+          </MainContent>
+        </Container>
+      </CharacterSheetSlot>
+      {isModalOpen && (
+        <UnequipCarousel cards={props.slot.equipped} onClose={closeModal} onSelectItem={handleSelectItem} />
+      )}
+    </>
   );
 };
 

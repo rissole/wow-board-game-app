77085dbfc57edb817f3709b192764c1aa1fcfccd
rissole <2404098+rissole@users.noreply.github.com>
2022-05-22 00:13:25 +1000

    allow equipping cards. also added a truly atrocious toast experience (#33)
    * allow equipping cards. also added a truly atrocious toast experience
    
    * Equip carousel says Equip

--- src/components/EmptyCharacterSheetSlot/index.tsx
+++ src/components/EmptyCharacterSheetSlot/index.tsx
@@ -1,42 +1,52 @@
-import { useCallback, useContext } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import styled from "styled-components";
 import SlotTypeIcons from "../SlotTypeIcons";
 import CharacterSheetSlot from "../BaseCharacterSheetSlot";
 import useFlipFlop from "../useFlipFlop";
 import EquipCarousel from "../CarouselEquip";
 import { GameContext } from "../GameProvider";
-import { getPowerByName } from "../../data-accessor";
 import { CardSlotMetadata, UniqueCardName } from "../../types";
+import { ALL_POWERS } from "../../data-accessor";
+import { getEquippableCards } from "../../util/data";
+import Toast from "../Toast";
 
 interface Props {
   cardSlotMetadata: CardSlotMetadata;
 }
 
 const EmptyCharacterSheetSlot = (props: Props) => {
-  const { purchasedCards } = useContext(GameContext);
-  const { value: isModalOpen, toggle: toggleModal } = useFlipFlop(false);
+  const { purchasedCards, equipCardToSlot, cardSlots } = useContext(GameContext);
+  const { value: isModalOpen, setOff: closeModal, toggle: toggleModal } = useFlipFlop(false);
+  const [toastText, setToastText] = useState<React.ReactNode>();
 
   const handleSelectItem = useCallback(
     (name: UniqueCardName) => {
-      const power = getPowerByName(name);
-      console.log("selected", power);
-      toggleModal();
+      equipCardToSlot(props.cardSlotMetadata.slotNumber, name);
+      closeModal();
     },
-    [toggleModal]
+    [closeModal, equipCardToSlot, props.cardSlotMetadata.slotNumber]
   );
 
-  const canEquipSomething = purchasedCards.length > 0;
+  const canEquipSomething = getEquippableCards(ALL_POWERS, purchasedCards, cardSlots).length > 0;
+  const showNothingToEquipToast = () =>
+    setToastText(
+      <span>
+        <p>Nothing to equip!</p>
+        <p>Try learning a spell or buying an item.</p>
+      </span>
+    );
 
   return (
     <>
       <CharacterSheetSlot
         style={{ justifyContent: "space-between" }}
-        onClick={canEquipSomething ? toggleModal : undefined}
+        onClick={canEquipSomething ? toggleModal : showNothingToEquipToast}
       >
         <SlotTypeIcons slotTypes={props.cardSlotMetadata.slotTypes} isEquippedSlot={false} />
         <AddIcon>+</AddIcon>
       </CharacterSheetSlot>
-      {isModalOpen && <EquipCarousel onClose={toggleModal} onSelectItem={handleSelectItem} />}
+      {isModalOpen && <EquipCarousel onClose={closeModal} onSelectItem={handleSelectItem} />}
+      <Toast durationMilliseconds={2500} text={toastText} />
     </>
   );
 };

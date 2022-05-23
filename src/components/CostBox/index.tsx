861277f2932ee5e41e5adb4052d3621f01157fbf
Blakeyboy <bonzaibrother@gmail.com>
2022-05-23 23:38:18 +1000

    better costbox style. it's epic

--- src/components/CostBox/index.tsx
+++ src/components/CostBox/index.tsx
@@ -5,20 +5,69 @@ interface Props {
 }
 
 const CostBox = ({ cost }: Props) => {
-  return <CostBoxInner>{cost}</CostBoxInner>;
+  return (
+    <CostBoxInner>
+      <Bolt />
+      <NumberWrapper>{cost}</NumberWrapper>
+    </CostBoxInner>
+  );
 };
 
+const NumberWrapper = styled.div`
+  position: relative;
+  top: -26px;
+`;
+
+const Bolt = styled.div`
+  position: relative;
+  top: -9px;
+  left: 6px;
+
+  opacity: 75%;
+  border: 4px solid rgba(0, 0, 0, 0);
+  border-bottom: 22px solid #ffdf00;
+  transform: rotateZ(-160deg);
+  padding: 2px;
+  width: 0;
+  height: 0;
+  margin: 0;
+  overflow: visible;
+  border-top: 0 solid;
+  border-radius: 0;
+  transform-origin: 50% 50% 0;
+
+  &:after {
+    content: "";
+    top: -9px;
+    left: -8px;
+    border: 5px solid rgba(0, 0, 0, 0);
+    border-bottom: 25px solid #ffdf00;
+    transform: rotateZ(4deg);
+    padding: 0;
+    width: 0;
+    height: 0;
+    position: absolute;
+    margin: 0;
+    overflow: visible;
+    border-top: 0 solid;
+    border-radius: 0;
+    transform-origin: 50% 50% 0;
+  }
+`;
+
 const CostBoxInner = styled.div`
-  background-color: #ffffff;
+  background: rgb(173, 53, 250);
+  background: linear-gradient(90deg, rgba(173, 53, 250, 1) 0%, rgba(196, 150, 217, 1) 76%);
   color: #000000;
-  width: 24px;
-  height: 36px;
+  width: 32px;
+  height: 32px;
   text-align: center;
   padding: 6px 0;
-  border-radius: 4px;
-  border: 1px solid black;
-  font-size: 18px;
-  line-height: 22px;
+  border-radius: 50%;
+  border: 2px outset #777777;
+  font-size: 22px;
+  font-weight: 700;
+  line-height: 14px;
 `;
 
 export default CostBox;

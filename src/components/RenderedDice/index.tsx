fa562dab9f3fbf184d5042faf7a748e3637d49a5
Ben Wong <benwong@atlassian.com>
2022-05-25 00:03:23 +1000

    yabai

--- src/components/RenderedDice/index.tsx
+++ src/components/RenderedDice/index.tsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 
 interface Props {
   diceColor: DiceColor;
+  diceColorSecondary?: DiceColor;
   numOfDice: number;
 }
 
-interface ContainerProps {
+interface DiceProps {
   diceColor: DiceColor;
+  diceColorSecondary?: DiceColor;
 }
 
 const diceColorToRenderColor = {
@@ -16,22 +18,51 @@ const diceColorToRenderColor = {
   blue: "#4a86e8",
 };
 
-const RenderedDice = (props: Props) => {
+const RenderedDice = ({ diceColor, diceColorSecondary, numOfDice }: Props) => {
+  const diceColors = { diceColor, diceColorSecondary };
   return (
-    <Container diceColor={props.diceColor}>
-      <div style={{ padding: "8px 0" }}>{props.numOfDice}</div>
+    <Container>
+      <Trapezium {...diceColors}>{numOfDice}</Trapezium>
     </Container>
   );
 };
 
-const Container = styled.div<ContainerProps>`
-  height: 0;
-  width: 36px;
-  border-bottom: 32px solid ${(props) => diceColorToRenderColor[props.diceColor]};
-  border-left: 12px solid transparent;
-  border-right: 12px solid transparent;
-  text-align: center;
+const Trapezium = styled.div<DiceProps>`
+  width: 20px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  perspective: 19px;
+  transform: translateY(-5px);
+  padding-top: 12px;
   font-weight: bold;
+  text-shadow: -1px 1px 0 #000, 1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000;
+
+  &:after {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: ${(props) =>
+      props.diceColorSecondary
+        ? `linear-gradient(90deg, ${diceColorToRenderColor[props.diceColor]} 50%, ${
+            diceColorToRenderColor[props.diceColorSecondary]
+          } 50%)`
+        : diceColorToRenderColor[props.diceColor]};
+    box-shadow: 0.1px -0.1px 1px 0.5px rgba(0, 0, 0, 0.5);
+    content: "";
+    left: 0;
+    top: 0;
+    z-index: -1;
+    transform: rotateX(25deg);
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  padding: 0 6px 0 6px;
 `;
 
 export default RenderedDice;
